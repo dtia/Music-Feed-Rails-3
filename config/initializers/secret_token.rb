63944bce9c39b749930c19051fd507e7aa3c663c
Derek Tia <dtia05@gmail.com>
2012-10-22 22:06:32 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicFeed::Application.config.secret_token = 'd21c71fe900bd085533b87aba3c4da92dc182169dab074c6c827746832c251edfba7c6ecd620a88010922a2ea6685415dda7958538fab3bbfa12649567d4f466'
